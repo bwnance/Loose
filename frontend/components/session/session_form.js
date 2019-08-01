08c5ba5010db8ec6f78800d0898003a3da067925
bwnance <bwnance@gmail.com>
2019-08-01 10:34:25 -0400

    Add Demo User functionality

--- frontend/components/session/session_form.js
+++ frontend/components/session/session_form.js
@@ -6,14 +6,17 @@ export default class SessionForm extends React.Component {
         this.state = this.props.formUser
         this.handleSubmit = this.handleSubmit.bind(this)
         this.handleInput = this.handleInput.bind(this)
+        this.demoLogin = this.demoLogin.bind(this)
         this.emailError = false;
         this.passwordError = false;
+        this.typingDemoUser = false;
     }
     componentDidMount() {
         document.body.classList.toggle('grey-bg');
     }
     handleSubmit(e) {
         e.preventDefault()
+        if (this.typingDemoUser) return
         this.emailError = !this.state.email
         this.passwordError = !this.state.password
         this.props.processForm(this.state)
@@ -25,6 +28,39 @@ export default class SessionForm extends React.Component {
         document.body.classList.toggle('grey-bg');
 
     }
+    demoLogin(){
+        if (this.typingDemoUser) return
+        const demoEmail = "demouser@loose.org"
+        const demoPassword = "demodemo"
+        
+        this.typingDemoUser = true;
+        let i = 0;
+        const that = this;
+        this.setState({ email: "", password: ""}, () => loginLoop())
+        function loginLoop(){
+            if (i < demoEmail.length) {
+                const char = demoEmail[i]
+                i++
+                return that.setState({ email: that.state.email + char }, () => setTimeout(loginLoop, 30))
+            }
+            else{
+                passwordLoop();
+            }
+        }
+        let j = 0;
+        function passwordLoop() {
+            if (j < demoPassword.length) {
+                const char = demoPassword[j]
+                j++
+                that.setState({ password: that.state.password + char }, () => setTimeout(passwordLoop, 30))
+            }
+            else
+            {
+                that.typingDemoUser = false;
+                that.handleSubmit({preventDefault: () => null });
+            }
+        }
+    } 
     handleInput(type) {
         return (e) => {
             this.setState({ [type]: e.target.value })
@@ -38,6 +74,7 @@ export default class SessionForm extends React.Component {
         const hasPasswordErrors = !!this.props.errors.password
         const hasPasswordsErrors = !!this.props.errors.passwords
         const anyErrors = hasFNErrors || hasEmailErrors || hasPasswordErrors;
+
 
         const formBody =  this.props.formType === 'signup' ? (
             <>
@@ -61,12 +98,13 @@ export default class SessionForm extends React.Component {
                 <h2 className="session-form-header">Sign In</h2>
 
                 <label htmlFor="email"/>
-                    <input id="email" className={`loose-text-input ${this.emailError ? "error-field" : ""}`} type="text" onChange={this.handleInput('email')} value={this.state.email} placeholder="Email"/>
+                    <input id="email" disabled={this.typingDemoUser} className={`typewriter loose-text-input ${this.emailError ? "error-field" : ""}`} type="text" onChange={this.handleInput('email')} value={this.state.email} placeholder="Email"/>
                     <div className="session-form-error" />
                 <label htmlFor="password" />
-                    <input id="password" className={`loose-text-input ${this.passwordError ? "error-field" : ""}`} type="password" onChange={this.handleInput('password')} value={this.state.password} placeholder="Password"/>
+                    <input id="password" disabled={this.typingDemoUser} className={`loose-text-input ${this.passwordError ? "error-field" : ""}`} type="password" onChange={this.handleInput('password')} value={this.state.password} placeholder="Password"/>
                     <div className="session-form-error" />
-                <input className="session-submit" type="submit" value="Sign In" />
+                    <input disabled={this.typingDemoUser} className="session-submit" type="submit" value="Sign In" />
+                    <input disabled={this.typingDemoUser} onClick={this.demoLogin}className="session-submit" type="button" value="Demo Login" />
             </>
         )
             return (
