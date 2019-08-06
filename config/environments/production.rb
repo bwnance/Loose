e5c2ceb5897599e02e0ebabbae579568c2aec37f
bwnance <bwnance@gmail.com>
2019-08-06 09:39:17 -0400

    fix fonts issue)
    a

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
 end
